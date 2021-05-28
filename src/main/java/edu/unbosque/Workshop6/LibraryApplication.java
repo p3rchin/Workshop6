0a4d89dc1adf17bcf9fad7e36e86d5c8f66e0dd7
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-05-27 20:11:05 -0500

    Creando el repositorio

---
+++ src/main/java/edu/unbosque/Workshop6/LibraryApplication.java
@@ -0,0 +1,9 @@
+package edu.unbosque.Workshop6;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class LibraryApplication extends Application {
+
+}
---
+++ src/main/java/resources/pojos/Employee.java
@@ -0,0 +1,32 @@
+package resources.pojos;
+
+public class Employee {
+
+    private String name;
+    private String role;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
