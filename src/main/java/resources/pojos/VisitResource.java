9e075914cc45d3bf269d90c822d4f2da32f48f14
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-05-29 17:57:27 -0500

    resources

--- src/main/java/resources/pojos/VisitResource.java
+++ src/main/java/resources/pojos/VisitResource.java
@@ -1,2 +1,26 @@
-package resources.pojos;public class VisitResource {
+package resources.pojos;
+
+import resources.pojos.Vet;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/visits/{id}")
+public class VisitResource {
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response modify(@PathParam("id") Integer id, Visit visit) {
+
+        return Response.ok()
+                .entity(visit)
+                .build();
+    }
+
+    @DELETE
+    public Response delete(@PathParam("id") Integer id) {
+
+        return Response.noContent()
+                .build();
+    }
 }
--- src/main/java/resources/pojos/VisitsResource.java
+++ src/main/java/resources/pojos/VisitsResource.java
@@ -1,2 +1,36 @@
-package resources.pojos;public class VisitsResource {
+
+import resources.pojos.Visit;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/visits")
+public class VisitsResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response list() {
+
+        List<Visit> visits = new ArrayList<Visit>();
+
+        visits.add(new Visit(1, "hola", "General", "El perro está vomitado", 2, 3));
+
+
+        return Response.ok()
+                .entity(visits)
+                .build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Visit visit) {
+
+        return Response.status(Response.Status.CREATED)
+                .entity(visit)
+                .build();
+    }
 }
