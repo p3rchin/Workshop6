02c5db11edc9bfa0bd9b3744361bbb9f451cdea2
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-05-29 17:14:03 -0500

    Creando los resource de Vet

--- src/main/java/resources/VetResource.java
+++ src/main/java/resources/VetResource.java
@@ -1,4 +1,26 @@
 package resources;
 
+import resources.pojos.Vet;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/vets/{username}")
 public class VetResource {
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response modify(@PathParam("username") String username, Vet vet) {
+
+        return Response.ok()
+                .entity(vet)
+                .build();
+    }
+
+    @DELETE
+    public Response delete(@PathParam("username") String username) {
+
+        return Response.noContent()
+                .build();
+    }
 }
--- src/main/java/resources/VetsResource.java
+++ src/main/java/resources/VetsResource.java
@@ -1,4 +1,38 @@
 package resources;
 
+import resources.pojos.Owner;
+import resources.pojos.Vet;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/vets")
 public class VetsResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response list() {
+
+        List<Vet> vets = new ArrayList<Vet>();
+
+        vets.add(new Vet("Focus", "Robinson Solano", "robinson21@gmail.com", "Barranquilla"));
+
+
+        return Response.ok()
+                .entity(vets)
+                .build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Owner owner) {
+
+        return Response.status(Response.Status.CREATED)
+                .entity(owner)
+                .build();
+    }
 }
