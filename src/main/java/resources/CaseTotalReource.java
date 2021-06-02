1c24ec7e16477d1d80abb16f56fa71772f6f3238
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2021-06-02 14:39:29 -0500

    total cases

--- src/main/java/resources/CaseTotalReource.java
+++ src/main/java/resources/CaseTotalReource.java
@@ -1,6 +1,9 @@
 package resources;
 
 import resources.pojos.Case;
+import resources.pojos.Owner;
+import resources.pojos.TotalCases;
+import resources.pojos.TotalOwners;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,28 +14,35 @@ import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
-@Path("/cases/{parametro}")
+@Path("/cases")
 public class CaseTotalReource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response list(@PathParam("parametro") String parametro) {
+    public Response list() {
 
         List<Case> cases = new ArrayList<Case>();
-        List<Case> cases1 = new ArrayList<Case>();
+        List<TotalCases> totalCase = new ArrayList<TotalCases>();
+
+        String[] casos = {"PERDIDA", "ROBO", "FALLECIMIENTO"};
         Integer total = 0;
 
         cases.add(new Case(1,"Created", "PERDIDA", "Es negra con manchas blancas", 1));
         cases.add(new Case(1,"Created", "ROBO", "Es negra con manchas blancas", 1));
         cases.add(new Case(1,"Created", "ROBO", "Es negra con manchas blancas", 1));
 
-        for (int i = 0; i < cases.size(); i++) {
-            if(parametro.equals(cases.get(i).getType())){
-                cases1.add(cases.get(i));
+        for (int i = 0; i < casos.length; i++) {
+            List<Case> cases1 = new ArrayList<Case>();
+            for (int j = 0; j < cases.size(); j++) {
+                if (casos[i].equals(cases.get(j).getType())){
+                    cases1.add(cases.get(j));
+                }
             }
-        }
-        total = cases1.size();
+
+            totalCase.add(new TotalCases("total de casos registrados por " + casos[i] + " es de " +(cases1.size()), cases1));
+            }
+
         return Response.ok()
-                .entity(total)
+                .entity(totalCase)
                 .build();
     }
 }
