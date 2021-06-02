d5c792d1c17016d65c0adcdd16a4b1a9f9d3fc00
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2021-06-01 23:28:35 -0500

    total owners final

--- src/main/java/resources/OwnerTotalResource.java
+++ src/main/java/resources/OwnerTotalResource.java
@@ -21,36 +21,24 @@ public class OwnerTotalResource {
 
         List<Owner> owners = new ArrayList<Owner>();
         List<TotalOwners> totalOwners = new ArrayList<TotalOwners>();
-        List<Owner> owners1 = new ArrayList<Owner>();
-        String[] neighborhood = {"Antonio Nariño", "Barrios Unidos", "Bosa", "Chapinero", "Ciudad Bolivar", "Engativá", "Fontibón", "Kennedy", "La Candelaria", "Los Martires," +
-                "Puente Aranda", "Rafael Uribe Uribe", "San Cristóbal", "Santa Fe", "Suba", "Sumapaz", "Teusaquillo", "Tunjuelito", "Usaquen", "Usme"};
+
+        String[] neighborhood = {"Antonio Nariño", "Barrios Unidos", "Bosa", "Chapinero", "Ciudad Bolivar", "Engativá", "Fontibón", "Kennedy", "La Candelaria", "Los Martires", "Puente Aranda", "Rafael Uribe Uribe", "San Cristóbal", "Santa Fe", "Suba", "Sumapaz", "Teusaquillo", "Tunjuelito", "Usaquen", "Usme"};
         Integer total = 0;
         owners.add(new Owner("perchin", 1, "Fabián Gómez", "hola2@hotmail.com", "Bogotá", null));
         owners.add(new Owner("perchin", 1, "Fabián Gómez", "hola2@hotmail.com", "Bogotá", null));
         owners.add(new Owner("perchin", 2, "Robinson Gutierrez", "hola2@hotmail.com", "Usaquen", null));
         owners.add(new Owner("perchin", 2, "Robinson Gutierrez", "hola2@hotmail.com", "Usaquen", null));
 
-        for (int i = 0; i < owners.size() ; i++) {
-            if (neighborhood[18].equals(owners.get(i).getNeighborhood())) {
-                 owners1.add(owners.get(i));
-
+        for (int i = 0; i < neighborhood.length; i++) {
+            List<Owner> owners1 = new ArrayList<Owner>();
+            for (int j = 0; j < owners.size(); j++) {
+                if (neighborhood[i].equals(owners.get(j).getNeighborhood())){
+                    owners1.add(owners.get(j));
+                }
             }
-        }
-           totalOwners.add(new TotalOwners("total "+ owners1.get(0).getNeighborhood() + " "+owners1.size(), owners1));
 
-//        for (int i = 0; i < neighborhood.length; i++) {
-//            for (int j = 0; j < owners.size(); j++) {
-//                if (neighborhood[i].equals(owners.get(j).getNeighborhood())){
-//                    List<Owner> owners1 = new ArrayList<Owner>();
-//                    owners1.add(owners.get(j));
-//                    totalOwners.add(new TotalOwners("total "+(j), owners1));
-//                }else{
-//                    List<Owner> owners1 = new ArrayList<Owner>();
-//                    totalOwners.add(new TotalOwners("total "+(0), owners1));
-//                }
-//            }
-
-//            }
+            totalOwners.add(new TotalOwners("total duenios por localidad " + neighborhood[i] + " es de " +(owners1.size()), owners1));
+            }
 
         return Response.ok()
                 .entity(totalOwners)
